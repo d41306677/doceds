f1921e7abb4945e7b583c085e1da8811b042f9b6
d41306677 <suresh.shanmugam@devry.edu>
2024-10-08 17:33:53 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -17,4 +17,26 @@ export default async function decorate(block) {
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
+
+   // Select the target element you want to replace
+const targetElement = document.querySelector('.default-content-wrapper');
+ 
+// Create a new div element
+const newDiv = document.createElement('div');
+newDiv.className = 'default-content-wrapper';
+ 
+// Create a new img element for the SVG
+const newImage = document.createElement('img');
+newImage.className ="logo"
+newImage.src = 'https://www.devry.edu/content/dam/devry_edu/svg/graphics/outlined/devry-edu/headerlogos/large/Header-Logo-DeVryEdu-Large.svg';
+newImage.alt = 'DeVry University Logo';
+newImage.style.width = '240px'; // Adjust the width as per your needs
+ 
+// Append the p and img elements to the div
+newDiv.appendChild(newImage);
+ 
+// Replace the old content with the new div
+if (targetElement) {
+    targetElement.replaceWith(newDiv);
+} 
 }
