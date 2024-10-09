91d227eeb39d04e4f20c118873305bad038d3fde
d41306677 <suresh.shanmugam@devry.edu>
2024-10-09 15:42:35 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -6,7 +6,21 @@ import { loadFragment } from '../fragment/fragment.js';
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-    
+
+const newDiv = document.createElement('div');
+newDiv.className = 'default-content-wrapper';
+ 
+// Create a new img element for the SVG
+const newImage = document.createElement('img');
+newImage.className ="logo"
+newImage.src = 'https://www.devry.edu/content/dam/devry_edu/svg/graphics/outlined/devry-edu/headerlogos/large/Header-Logo-DeVryEdu-Large.svg';
+newImage.alt = 'DeVry University Logo';
+newImage.style.width = '240px'; // Adjust the width as per your needs
+ 
+// Append the p and img elements to the div
+newDiv.appendChild(newImage);
+  
+  
   // load footer as fragment
   const footerMeta = getMetadata('footer');
   const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
@@ -19,6 +33,20 @@ export default async function decorate(block) {
   block.append(hrtag)
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+
+  const newDiv = document.createElement('div');
+newDiv.className = 'default-content-wrapper';
+ 
+// Create a new img element for the SVG
+const newImage = document.createElement('img');
+newImage.className ="logo"
+newImage.src = 'https://www.devry.edu/content/dam/devry_edu/svg/graphics/outlined/devry-edu/headerlogos/large/Header-Logo-DeVryEdu-Large.svg';
+newImage.alt = 'DeVry University Logo';
+newImage.style.width = '240px'; // Adjust the width as per your needs
+ 
+// Append the p and img elements to the div
+newDiv.appendChild(newImage);
+  block.append(newDiv);
 
   block.append(footer);
 }
