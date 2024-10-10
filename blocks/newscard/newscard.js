35261b069b7a4553f818208a8b54265067224418
d41306677 <suresh.shanmugam@devry.edu>
2024-10-10 18:18:21 +0530

    Update newscard.js

--- blocks/newscard/newscard.js
+++ blocks/newscard/newscard.js
@@ -1,4 +1,13 @@
+
+ import { h, render } from 'https://esm.sh/preact';
+
+  // Create your app
+  
+
 export default async function createNewsFilter(block) {
+  const app = h('h1', null, 'Hello World!');
+
+  render(app, document.body);
     // Create the news filter wrapper div
     const newsFilter = document.createElement('div');
     newsFilter.className = 'news-filter';
