93dc0e153fc7097970967f672eb574d17d0264ef
d41306677 <suresh.shanmugam@devry.edu>
2024-10-14 17:19:23 +0530

    Update carousal.js

--- blocks/carousal/carousal.js
+++ blocks/carousal/carousal.js
@@ -8,26 +8,26 @@ const html = htm.bind(h);
 function Header({ title }) {
   return html`
     <header class="header">
-      <div class="header__logo">
-        <h1 class="header__title">${title}</h1>
-      </div>
-      <nav class="header__nav">
-        <ul class="header__menu">
-          <li class="header__menu-item">
-            <a href="/home" class="header__menu-link">Home</a>
-          </li>
-          <li class="header__menu-item">
-            <a href="/about" class="header__menu-link">About</a>
-          </li>
-          <li class="header__menu-item">
-            <a href="/services" class="header__menu-link">Services</a>
-          </li>
-          <li class="header__menu-item">
-            <a href="/contact" class="header__menu-link">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+  <div class="header__logo">
+    <h1 class="header__title">MyApp</h1>
+  </div>
+  <nav class="header__nav">
+    <ul class="header__menu">
+      <li class="header__menu-item">
+        <a href="/home" class="header__menu-link">Home</a>
+      </li>
+      <li class="header__menu-item">
+        <a href="/about" class="header__menu-link">About</a>
+      </li>
+      <li class="header__menu-item">
+        <a href="/services" class="header__menu-link">Services</a>
+      </li>
+      <li class="header__menu-item">
+        <a href="/contact" class="header__menu-link">Contact</a>
+      </li>
+    </ul>
+  </nav>
+</header>
   `;
 }
 
